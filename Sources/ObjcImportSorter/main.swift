f78186123c2f8230f71471d7197a4ac2a2accb4a
Marcus Wu <marcus.wu0814@gmail.com>
2018-05-07 10:45:11 +0800

    Implement given type to sort, support .h and .m now

--- Sources/ObjcImportSorter/main.swift
+++ Sources/ObjcImportSorter/main.swift
@@ -1,10 +1,16 @@
 import Commander
 import ObjcImportSorterCore
+import Rainbow
 
 Group {
     
-    $0.command("sort") {
-        ImportSorter().sort(by: .h)
+    $0.command("sort") { (fileType: String) in
+        guard let fileType = FileType(rawValue: "*." + fileType) else {
+            print("Wrong given type".red)
+            return
+        }
+        
+        ImportSorter().sort(by: fileType)
     }
     
 }.run()
